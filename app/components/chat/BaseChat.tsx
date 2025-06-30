3d5050c9910f3b1efda9ce8e40174c1f22e44df3
雲風 <wangkanghk@gmail.com>
2025-06-30 19:39:04 +1000

    Update BaseChat.tsx

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -345,10 +345,10 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
             {!chatStarted && (
               <div id="intro" className="mt-[16vh] max-w-2xl mx-auto text-center px-4 lg:px-0">
                 <h1 className="text-3xl lg:text-6xl font-bold text-bolt-elements-textPrimary mb-4 animate-fade-in">
-                  AI网页编程/Where ideas begin
+                  免费AI网页编程
                 </h1>
                 <p className="text-md lg:text-xl mb-8 text-bolt-elements-textSecondary animate-fade-in animation-delay-200">
-                  把你的创意3分钟变成网页.
+                  把你的创意3分钟变成网页
                 </p>
               </div>
             )}
